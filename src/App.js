19f531d8e28721b620e58e72009f966290086a01
ajay <ajaybaviskar830@gmail.com>
2023-05-24 23:46:54 +0530

    Inprove more features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <section>
+      <form >
+            <h1>Add Task and Description</h1>
+            <div class="hero">
+              <input type="text" name="task" placeholder="Enter Task" required />
+               {/* <textarea className="form-control" value={text}   id="myBox" rows="8"></textarea> */}
+              <textarea placeholder="Enter Description"  id="myBox" rows="8"></textarea>
+              <button class="btn">Add Task</button>
+            </div>
+            <span id="msg"></span>
+        </form>
+
+      </section>
+      
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +31,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
